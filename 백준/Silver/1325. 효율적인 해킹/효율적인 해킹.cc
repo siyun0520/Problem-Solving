50775cfce83f29342e57ed9c6797306d14dabb17
siyun <syun5520@naver.com>
2026-04-21 11:50:49 +0200

    [Silver I] Title: 효율적인 해킹, Time: 2848 ms, Memory: 3512 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -1,52 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-using namespace std;
-
-vector<vector<int>> A;
-vector<int> answer;
-vector<bool> visited;
-
-void bfs(int index) {
-    queue<int> qu;
-    qu.push(index);
-    visited[index] = true;
-    while (!qu.empty()) {
-        int now_node = qu.front();
-        qu.pop();
-        for (int i : A[now_node]) {
-            if (visited[i] == false) {
-                visited[i] = true;
-                answer[i]++;
-                qu.push(i);
-            }
-        }
-    }
-}
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-
-    long n, m;
-    cin >> n >> m;
-    A.resize(n + 1);
-    answer.resize(n + 1);
-    for (int i = 0; i < m; i++) {
-        int s, e;
-        cin >> s >> e;
-        A[s].push_back(e);
-    }
-    visited.resize(n + 1);
-    for (int i = 0; i <= n; i++) {
-        fill(visited.begin(), visited.end(), false);
-        bfs(i);
-    }
-    int max_num = 0;
-    for (int i = 1; i <= n; i++) {
-        max_num = max(max_num, answer[i]);
-    }
-    for (int i = 1; i <= n; i++) {
-        if (answer[i] == max_num) {
-            cout << i << " ";
-        }
-    }
-}
+<html>
+<head><title>502 Bad Gateway</title></head>
+<body>
+<center><h1>502 Bad Gateway</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
